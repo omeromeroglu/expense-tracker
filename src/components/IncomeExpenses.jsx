6ad57279cadced614243995f034ee38536d6e753
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-02-16 22:54:14 +0300

    completed version

---
+++ src/components/IncomeExpenses.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react"
+import { GlobalContext } from "../context/GlobalState"
+
+const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext)
+  const income = transactions
+    .filter((transaction) => transaction.amount > 0)
+    .map((transaction) => transaction.amount)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2)
+  const expenses = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .map((transaction) => transaction.amount)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2)
+
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p className="money plus">{income}</p>
+      </div>
+      <div>
+        <h4>Expenses</h4>
+        <p className="money minus">{expenses}</p>
+      </div>
+    </div>
+  )
+}
+
+export default IncomeExpenses
